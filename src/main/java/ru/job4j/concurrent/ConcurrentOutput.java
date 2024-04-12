3df8aeb227b33f2ed755721fed4512cee9149811
aleksandrblagov <blagov13rus@gmail.com>
2024-04-12 16:37:02 +0300

    3. Состояние нити. [#229175]

--- src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
+++ src/main/java/ru/job4j/concurrent/ConcurrentOutput.java
@@ -3,13 +3,20 @@ package ru.job4j.concurrent;
 public class ConcurrentOutput {
     public static void main(String[] args) {
         Thread another = new Thread(
-                () -> System.out.println(Thread.currentThread().getName())
+                () -> {}
         );
-        Thread second = new Thread(
-                () -> System.out.println(Thread.currentThread().getName())
-        );
+        System.out.println(another.getName());
         another.start();
+        Thread second = new Thread(
+                () -> {}
+        );
+        System.out.println(second.getName());
         second.start();
-        System.out.println(Thread.currentThread().getName());
+        while (another.getState() != Thread.State.TERMINATED
+                || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("thread 1: " + another.getState());
+            System.out.println("thread 2: " + second.getState());
+        }
+        System.out.println("work TERMINATED");
     }
 }
