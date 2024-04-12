f1436068cc9de2201a97121ca265ad18354f2ebb
aleksandrblagov <blagov13rus@gmail.com>
2024-04-12 18:12:11 +0300

    5. Прерывание нити [#1019]

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,28 @@
+package ru.job4j.concurrent;
+
+public class ConsoleProgress implements Runnable {
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(5000);
+        progress.interrupt();
+    }
+
+    @Override
+    public void run() {
+        try {
+            int i = 0;
+            char[] process = new char[]{'-', '\\', '|', '/'};
+            while (!Thread.currentThread().isInterrupted()) {
+                if (i == process.length) {
+                    i = 0;
+                }
+                System.out.print("\r load: " + process[i++]);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            System.out.println("finish");
+        }
+    }
+}
+
