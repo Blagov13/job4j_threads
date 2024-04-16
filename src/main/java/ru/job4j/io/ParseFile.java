f9ed81d55a95465f6f755d1bcd679bdaabc2ed1b
aleksandrblagov <blagov13rus@gmail.com>
2024-04-16 14:16:06 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102]

---
+++ src/main/java/ru/job4j/io/ParseFile.java
@@ -0,0 +1,28 @@
+package ru.job4j.io;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.util.function.IntPredicate;
+import java.util.function.Predicate;
+
+public class ParseFile {
+    private final File file;
+
+    public ParseFile(File file) {
+        this.file = file;
+    }
+
+    public String getContent(Predicate<Character> filter) throws IOException {
+        try {
+            return Files.readString(file.toPath())
+                    .chars()
+                    .filter((IntPredicate) filter)
+                    .mapToObj(c -> (char) c)
+                    .map(String::valueOf)
+                    .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
+                    .toString();
+        } catch (IOException e) {
+            throw new IOException();
+        }
+    }
+}
---
+++ src/main/java/ru/job4j/io/ParseFileWriter.java
@@ -0,0 +1,26 @@
+package ru.job4j.io;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class ParseFileWriter {
+    private final File file;
+
+    public ParseFileWriter(File file) {
+        this.file = file;
+    }
+
+    public void saveContent(String content) throws IOException {
+        try (
+                OutputStream o = new FileOutputStream(file)
+        ) {
+            for (int i = 0; i < content.length(); i++) {
+                o.write(content.charAt(i));
+            }
+        } catch (IOException e) {
+            throw new IOException();
+        }
+    }
+}
