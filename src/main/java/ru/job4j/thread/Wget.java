74272c929e433a49c1a3c436b6cc28b793668e1a
aleksandrblagov <blagov13rus@gmail.com>
2024-04-17 15:33:37 +0300

    8. Скачивание файла с ограничением. [#144271 #491399]

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -2,35 +2,38 @@ package ru.job4j.thread;
 
 import java.io.*;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private final String locationFile;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String locationFile) {
         this.url = url;
         this.speed = speed;
+        this.locationFile = locationFile;
     }
 
     @Override
     public void run() {
-        Path file = Paths.get("output.data");
         try (
-                InputStream inputStream = new URL(url).openStream();
-                OutputStream outputStream = Files.newOutputStream(file)
+                BufferedInputStream inputStream = new BufferedInputStream(new URL(url).openStream());
+                FileOutputStream outputStream = new FileOutputStream(locationFile)
         ) {
             byte[] buffer = new byte[1024];
             int bytesRead;
-            while ((bytesRead = inputStream.read(buffer)) != -1) {
+            int downloadData = 0;
+            long timeBefore = System.currentTimeMillis();
+            while ((bytesRead = inputStream.read(buffer, 0, 1024)) != -1) {
                 outputStream.write(buffer, 0, bytesRead);
-                long elapsedTime = System.nanoTime() - startTime;
-                double elapsedSeconds = elapsedTime / 1_000_000_000.0;
-                long timeForDownload = bytesRead / speed;
-                if (timeForDownload > elapsedSeconds) {
-                    Thread.sleep((long) (timeForDownload - elapsedSeconds));
+                downloadData += bytesRead;
+                if (downloadData >= speed * 1_000_000) {
+                    long time = System.currentTimeMillis() - timeBefore;
+                    if (time < 1_000_000_000) {
+                        Thread.sleep(1000 - time / 1_000_000);
+                    }
+                    timeBefore = System.currentTimeMillis();
+                    downloadData = 0;
                 }
             }
         } catch (IOException | InterruptedException e) {
@@ -38,16 +41,19 @@ public class Wget implements Runnable {
         }
     }
 
-    private final long startTime = System.nanoTime();
-
     public static void main(String[] args) throws InterruptedException {
-        if (args.length != 2) {
+        if (args.length != 3) {
             throw new IllegalArgumentException();
         }
-        String url = args[0];
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
-        wget.start();
-        wget.join();
+        try {
+            String url = args[0];
+            int speed = Integer.parseInt(args[1]);
+            String fileLocation = args[2];
+            Thread wget = new Thread(new Wget(url, speed, fileLocation));
+            wget.start();
+            wget.join();
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
     }
 }
