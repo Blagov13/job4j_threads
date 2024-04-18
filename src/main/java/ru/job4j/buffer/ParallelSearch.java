f329d68c17c383b084977702c563cc633f3ffc74
aleksandrblagov <blagov13rus@gmail.com>
2024-04-18 12:11:34 +0300

    2. Обеспечить остановку потребителя. [#66825 #491788]

--- src/main/java/ru/job4j/buffer/ParallelSearch.java
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -3,37 +3,41 @@ package ru.job4j.buffer;
 import ru.job4j.consumer.SimpleBlockingQueue;
 
 public class ParallelSearch {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(10);
         final Thread consumer = new Thread(
                 () -> {
                     while (!Thread.currentThread().isInterrupted()) {
                         try {
-                            System.out.println(queue.poll());
+                            Integer value = queue.poll();
+                            if (value == null) {
+                                break;
+                            }
+                            System.out.println(value);
                         } catch (InterruptedException e) {
                             Thread.currentThread().interrupt();
+                            break;
                         }
                     }
                 }
         );
         consumer.start();
-        new Thread(
+        Thread producer = new Thread(
                 () -> {
                     for (int index = 0; index != 3; index++) {
                         try {
                             queue.offer(index);
-                        } catch (InterruptedException e) {
-                            throw new RuntimeException(e);
-                        }
-                        try {
                             Thread.sleep(500);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                            break;
                         }
                     }
-                    consumer.interrupt();
                 }
-
-        ).start();
+        );
+        producer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
     }
 }
