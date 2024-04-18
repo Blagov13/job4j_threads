e7ba9cf818d79e2e86d14af7bd2c257ac556870e
aleksandrblagov <blagov13rus@gmail.com>
2024-04-18 12:45:16 +0300

    3. Junit тест для блокирующей очереди. [#68589]

--- src/test/java/ru/job4j/consumer/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/consumer/SimpleBlockingQueueTest.java
@@ -37,4 +37,33 @@ class SimpleBlockingQueueTest {
         consumer.join();
         assertEquals(Arrays.asList(0, 1, 2, 3, 4), buffer);
     }
+
+    @Test
+    public void whenFetchAllThenGetItString() throws InterruptedException {
+        SimpleBlockingQueue<String> queue = new SimpleBlockingQueue<>(10);
+        List<String> buffer = new LinkedList<>();
+        Thread producer = new Thread(() -> {
+            for (int i = 0; i < 3; i++) {
+                try {
+                    queue.offer("String " + i);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        Thread consumer = new Thread(() -> {
+            while (buffer.size() < 3) {
+                try {
+                    buffer.add(queue.poll());
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        });
+        producer.start();
+        consumer.start();
+        producer.join();
+        consumer.join();
+        assertEquals(Arrays.asList("String 0", "String 1", "String 2"), buffer);
+    }
 }
