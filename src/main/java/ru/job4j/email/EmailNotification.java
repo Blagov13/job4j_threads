59e770371cfc67e712ce53d42854ab37e2cfd37d
aleksandrblagov <blagov13rus@gmail.com>
2024-04-19 17:51:31 +0300

    2. ExecutorService рассылка почты. [#63097]

---
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -0,0 +1,29 @@
+package ru.job4j.email;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class EmailNotification {
+    private final ExecutorService pool;
+
+    public EmailNotification() {
+        this.pool = Executors.newFixedThreadPool(10);
+    }
+
+    public void emailTo(User user) {
+        pool.submit(() -> {
+            String subject = "Notification " + user.getUsername()
+                    + " to email " + user.getEmail();
+            String body = "Add a new event to " + user.getUsername();
+            send(subject, body, user.getEmail());
+        });
+    }
+
+    public void send(String subject, String body, String email) {
+        System.out.println(" ");
+    }
+
+    public void close() {
+        pool.shutdown();
+    }
+}
